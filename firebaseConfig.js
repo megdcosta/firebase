72167417044129d946ab5764cc721ff2b697e548
Megan D Costa <mdcosta@uoguelph.ca>
2023-11-22 19:25:02 -0500

    completed firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,30 @@
+// get firebase modules via Javascript SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+
+// apiKey: Public API key to interact with Firebase services
+// authDomain: URL used for user authentication services
+// projectId: Project ID for your Firebase application
+// storageBucket: URL for file storage within your application
+// messagingSenderId: Sender ID for sending messages via Firebase Cloud Messaging
+// appId: Application ID for the Firebase app, it is unique to this application
+
+// STEP 1: Declare Firebase Environment Variables  
+const firebaseConfig = {
+    apiKey: "AIzaSyDl3ENlrXkq_yzbWbpDiKKOkhz0zdtOeeA",
+    authDomain: "learningwithfirebase-65cc0.firebaseapp.com",
+    projectId: "learningwithfirebase-65cc0",
+    storageBucket: "learningwithfirebase-65cc0.appspot.com",
+    messagingSenderId: "694571286889",
+    appId: "1:694571286889:web:2786ccbd15b15d53433df8"
+  };
+
+// initialze firebase in our project by passing in our environment variables to the `initializeApp` method
+const app = initializeApp(firebaseConfig);
+
+// initialze firebase authentication by passing in firebase `app` instance we delared on the line above
+export const auth = getAuth(app);
+
+// initialze firestore by passing in firebase `app` instance we delared
+export const db = getFirestore(app);
